157eb2e5578be2761ae1a3a353cd7f57223d6d47
Amankos Ansar <zugzwanggg0@gmail.com>
2025-04-20 17:45:08 +0500

    google oauth2 passport auth set up

--- backend/index.js
+++ backend/index.js
@@ -3,20 +3,22 @@ import dotenv from "dotenv";
 dotenv.config();
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import passport from "passport";
 
 import { authRoute } from "./routes/authRoute.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 const corsOptions = {
   origin: [`${process.env.FRONTEND_BASE_URL}`],
   credentials: true
-}
+};
+app.use(cors(corsOptions));
+app.use(passport.initialize());
 
-app.use(cors(corsOptions))
 
 app.use('/api', authRoute);
 
