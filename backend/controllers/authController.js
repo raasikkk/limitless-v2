b8b0c72e88a6d3dddc46fe161f047dfca31e5ed8
Amankos Ansar <zugzwanggg0@gmail.com>
2025-04-20 13:03:01 +0500

    initial register api

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import { db } from "../db.js";
 import bcryptjs from "bcryptjs";
+import validator from "validator";
 
 const JWT_SECRET = process.env.JWT_SECRET ? process.env.JWT_SECRET : 'best_secret_2025';
 
@@ -47,6 +48,70 @@ export const login = async (req,res) => {
 
   } catch (error) {
     console.log('Error at login:', error);
+    res.status(500).send(error)
+  }
+}
+
+export const register = async (req,res) => {
+  try {
+
+    const {email, username, password, repPassword} = req.body;
+
+    if (!email || !username || !password || !repPassword) {
+      return res.status(400).json({
+        message: "Fields cannot be empty!"
+      })
+    }
+
+    const checkUser = await db.query("SELECT * FROM users WHERE username = $1 OR email = $2", [username, email]);
+    if (checkUser.rows.length > 0) {
+      return res.status(400).json({
+        message: "User with this email or username already exists."
+      })
+    }
+
+    const isPasswordStrong = validator.isStrongPassword(password, {
+      minLength: 6,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1
+    })
+
+    if (password !== repPassword) {
+      return res.status(400).json({
+        message: "Passwords are not matching!"
+      })
+    }
+
+    if (!isPasswordStrong) {
+      return res.status(400).json(
+        {
+          message: "Weak password!"
+        }
+      )
+    }
+
+    const genSalt = await bcryptjs.genSalt();
+    const hashPassword = await bcryptjs.hash(password, genSalt);
+
+    const newUser = await db.query("INSERT INTO users (email, username, password) VALUES ($1, $2, $3) RETURNING *", [email, username, hashPassword]);
+
+    const payload = newUser.rows[0];
+    const token = jwt.sign(payload, JWT_SECRET);
+
+    const sevenDays = 7 * 24 * 60 * 60 * 1000;
+    res.cookies('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: sevenDays
+    });
+    
+    res.json({
+      message: "User created succesfully."
+    })
+    
+  } catch (error) {
+    console.log('Error at register:', error);
     res.status(500).send(error)
   }
 }
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { login } from "../controllers/authController.js";
+import { login, register } from "../controllers/authController.js";
 
 export const authRoute = Router();
 
-authRoute.post('/register', ()=> {});
+authRoute.post('/register', register);
 authRoute.post('/login', login);
 authRoute.get('/logout', ()=> {});
 authRoute.get('/isLogged', ()=> {});
