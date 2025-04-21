c8461f021c98998aa2e879a85b9c6821a9383590
zhankeldiulyrasultop1@gmail.com <zhankeldiulyrasultop1@gmail.com>
2025-04-21 23:12:01 +0500

    home page done

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,19 @@
+
+const Footer = () => {
+  return (
+    <footer className="">
+        <center>
+            <hr className="my-3 sm:mx-auto lg:my-6 text-center" />
+            <span className="block text-sm pb-4 text-gray-800 text-center">
+            © 2025{" "}
+            <a href="#" className="hover:underline">
+                Limitless
+            </a>
+            . All Rights Reserved.
+            </span>
+        </center>
+        </footer>
+  )
+}
+
+export default Footer
