2bc56ce317f8bc4f9a152ce6cec77ffe0a3c04b6
Amankos Ansar <zugzwanggg0@gmail.com>
2025-04-21 23:20:22 +0500

    initial followers api

--- backend/routes/followerRoute.js
+++ backend/routes/followerRoute.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { getUserFollowersById, getUserFollowingById } from "../controllers/followerController.js";
+import { followUser, getUserFollowersById, getUserFollowingById, unfollowUser } from "../controllers/followerController.js";
 import { checkAuth } from "../middleware/checkAuth.js";
 
 export const followerRoute = Router();
 
+
+followerRoute.post('/follow', followUser);
+followerRoute.delete('/unfollow/:user_id/:follower_id', unfollowUser);
 followerRoute.get('/followers/:user_id', checkAuth, getUserFollowersById);
 followerRoute.get('/following/:user_id', checkAuth, getUserFollowingById);
