f489605f381d55dbc2b865356d59eef13edcac60
Amankos Ansar <zugzwanggg0@gmail.com>
2025-04-24 13:50:13 +0500

    initial edit submission image api

--- backend/controllers/submissionController.js
+++ backend/controllers/submissionController.js
@@ -76,6 +76,57 @@ export const getSubmissions = async (req,res) => {
   }
 }
 
+export const editSubmissionImage = async (req,res) => {
+  try {
+
+    const {userId, competitionId} = req.params;
+    const date = new Date();
+    const file = req.file;
+    if (!file) return res.status(400).json({
+      message: "No image provided!"
+    })
+
+    const competition = await db.query("SELECT * FROM competitions WHERE id = $1", [competitionId]);
+    if (competition.rows.length <= 0) {
+      return res.status(400).json({
+        message: "Incorrect id or competition doesn't exist"
+      })
+    }
+
+    const submission = await db.query("SELECT * FROM submissions WHERE participant_id = $1 AND competition_id = $2 AND DATE(submited_date) = DATE($3)", [userId, competitionId, date]);
+    if (submission.rows.length <= 0) {
+      return res.status(400).json({
+        message: "Only today's submissions can be edited"
+      })
+    }
+
+    const publicId = `submission_user-${userId}_competition-${competitionId}_${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    await cloudinary.uploader.destroy(publicId);
+
+    await cloudinary.uploader.upload(
+      req.file.path,
+      {
+        folder: 'competitions/submissions',
+        public_id: publicId,
+        overwrite: true
+      },
+      async (err, res) => {
+        if (err) return res?.status(500).json({ error: 'Cloudinary upload failed' });
+
+        await db.query("UPDATE submissions SET image = $1 WHERE participant_id = $2 AND competition_id = $3", [res?.secure_url, userId, competitionId]);
+      }
+    )
+
+    res.json({
+      message: "Succesfully updated submission image"
+    })
+    
+  } catch (error) {
+    console.log('Error at editSubmissionImage:', error);
+    res.status(500).send(error)
+  }
+}
+
 export const editSubmissionText = async (req,res) => {
   try {
     const {userId, competitionId} = req.params;
