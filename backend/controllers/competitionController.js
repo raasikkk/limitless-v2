a598f4f64f1245770a907277f90a9698493ca82c
Amankos Ansar <zugzwanggg0@gmail.com>
2025-04-24 09:16:12 +0500

    basic competition participant actions

--- backend/controllers/competitionController.js
+++ backend/controllers/competitionController.js
@@ -63,3 +63,81 @@ export const getCategories = async (req,res) => {
     res.status(500).send(error)
   }
 }
+
+export const joinCompetition = async (req,res) => {
+  try {
+
+    const {competition_id, user_id} = req.body;
+
+    const competition = await db.query("SELECT private FROM competitions WHERE id = $1", [competition_id]);
+    const isPrivate = competition.rows[0].private;
+    if (isPrivate) {
+      return res.status(400).json({
+        message: "The competition is private."
+      })
+    }
+
+    const checkIsUserCreator = await db.query("SELECT user_id FROM competitions WHERE id = $1 AND user_id = $2", [competition_id, user_id]);
+    if (checkIsUserCreator.rows.length > 0) {
+      return res.status(400).json({
+        message: "Already participating in the competition."
+      })
+    }
+
+    const checkUser = await db.query("SELECT participants.user_id FROM participants WHERE user_id = $1 AND competition_id = $2", [user_id, competition_id]);
+    if (checkUser.rows.length > 0) {
+      return res.status(400).json({
+        message: "Already participating in the competition."
+      })
+    }
+
+
+    await db.query("INSERT INTO participants (user_id, competition_id) VALUES ($1, $2)", [user_id, competition_id]);
+    res.json({
+      message: "Succesfully joined."
+    })
+
+  } catch (error) {
+    console.log('Error at joinCompetition:', error);
+    res.status(500).send(error)
+  }
+}
+
+export const quitCompetition = async (req, res) => {
+  try {
+    const {competition_id, user_id} = req.params;
+
+    const checkUser = await db.query("SELECT user_id FROM participants WHERE user_id = $1 AND competition_id = $2", [user_id, competition_id]);
+
+    if (checkUser.rows.length <= 0) {
+      return res.status(400).json({
+        message: "User is not participant of the competition"
+      })
+    }
+
+    await db.query("DELETE FROM participants WHERE user_id = $1 AND competition_id = $2", [user_id, competition_id]);
+    res.json({
+      message: "Succesfully quit."
+    })
+    
+  } catch (error) {
+    console.log('Error at quitCompetition:', error);
+    res.status(500).send(error)
+  }
+}
+
+export const getParticipants = async (req,res) => {
+  try {
+    const { competition_id } = req.params;
+
+    const participants = await db.query(`
+    SELECT users.id, users.avatar, users.username FROM competitions JOIN users ON users.id = competitions.user_id WHERE competitions.id = $1
+    UNION
+    SELECT users.id, users.avatar, users.username FROM participants JOIN users ON users.id = participants.user_id WHERE participants.competition_id = $1`, [competition_id]);
+
+    res.json(participants.rows);
+  } catch (error) {
+    console.log('Error at getParticipants:', error);
+    res.status(500).send(error)
+  }
+}
--- backend/routes/competitionRoute.js
+++ backend/routes/competitionRoute.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createCompetition, getCategories, getCompetitionById, getCompetitions } from "../controllers/competitionController.js";
+import { createCompetition, getCategories, getCompetitionById, getCompetitions, getParticipants, joinCompetition, quitCompetition } from "../controllers/competitionController.js";
 import { checkAuth } from "../middleware/checkAuth.js";
 
 
@@ -9,4 +9,9 @@ export const competitionRoute = Router();
 competitionRoute.post('/competitions', checkAuth, createCompetition);
 competitionRoute.get('/competitions', getCompetitions);
 competitionRoute.get('/competitions/:id', getCompetitionById);
+
+competitionRoute.post('/competitions/join', checkAuth, joinCompetition);
+competitionRoute.delete('/competitions/quit/:competition_id/:user_id', checkAuth, quitCompetition);
+competitionRoute.get('/competitions/:competition_id/participants', getParticipants);
+
 competitionRoute.get("/categories", getCategories);
