3d128483d9ee725823e6b6394a38caa5d34881d3
F1awless77 <yermekovaibar@gmail.com>
2025-04-24 11:37:49 +0500

    feat: Added answer grading by AI

--- backend/controllers/llmController.js
+++ backend/controllers/llmController.js
@@ -9,6 +9,12 @@ export const llmSuggestions = async (req, res) => {
   try {
     const requestTopic = req.body.topic;
 
+    if (!requestTopic) {
+      res.status(400).send({
+        message: "requestTopic must be provided",
+      });
+    }
+
     const response = await ai.models.generateContent({
       model: "gemini-2.0-flash",
       contents: `Hello, we are Limitless - a platform for students trying to improve their knowledge on certain topics by competing with their friends. We need to give our user 1 suggestion for a question in a quiz they are creating. Please put the question on the first row, and the answers from second to fifth row, on the sixth row mark correct answer with the index of the row do not put it in any brackets. Do not greet us, only give us the suggestions. The topic of the quiz is: ${requestTopic}`,
@@ -30,8 +36,67 @@ export const llmSuggestions = async (req, res) => {
 export const llmAnswerGrading = async (req, res) => {
   try {
     const { img_url, text_content } = req.body;
+
+    if (!img_url && !text_content) {
+      return res.status(400).send({
+        message: "img_url or text_content must be provided",
+      });
+    }
+
+    if (!img_url) {
+      const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: text_content,
+      });
+
+      return res.status(200).send({
+        content: response.text,
+      });
+    }
+
+    const response = await fetch(img_url);
+    const imageArrayBuffer = await response.arrayBuffer();
+    const base64ImageData = Buffer.from(imageArrayBuffer).toString("base64");
+
+    if (!text_content) {
+      const result = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: [
+          {
+            inlineData: {
+              mimeType: "image/jpeg",
+              data: base64ImageData,
+            },
+          },
+          {
+            text: "Analyse this image and respond however you want to",
+          },
+        ],
+      });
+
+      return res.status(200).send({
+        content: result.candidates[0].content.parts[0].text,
+      });
+    }
+
+    const result = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: [
+        {
+          inlineData: {
+            mimeType: "image/jpeg",
+            data: base64ImageData,
+          },
+        },
+        { text: text_content },
+      ],
+    });
+
+    return res.status(200).send({
+      content: result.candidates[0].content.parts[0].text,
+    });
   } catch (error) {
     console.log(`Error occured at route '/llm/grading': ${error}`);
-    res.status(500).send({ error_message: error });
+    return res.status(500).send({ error_message: error });
   }
-}
+};
--- backend/routes/llmRouter.js
+++ backend/routes/llmRouter.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { llmSuggestions, llmAnswerGrading } from "../controllers/llmController.js";
+import {
+  llmSuggestions,
+  llmAnswerGrading,
+} from "../controllers/llmController.js";
 import { checkAuth } from "../middleware/checkAuth.js";
 
 export const llmRouter = Router();
 
 llmRouter.get("/llm/suggestions", checkAuth, llmSuggestions);
-llmRouter.get("/llm/grading", checkAuth, llmSuggestions);
+llmRouter.get("/llm/grading", llmAnswerGrading);
