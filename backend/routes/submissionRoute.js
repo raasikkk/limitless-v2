8a9a58b1159be5fc16ba6b9f1c4b6281c1b7ebcf
Amankos Ansar <zugzwanggg0@gmail.com>
2025-04-24 19:32:46 +0500

    Changed features

--- backend/routes/submissionRoute.js
+++ backend/routes/submissionRoute.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { checkAuth } from "../middleware/checkAuth.js";
-import { deleteSubmission, editSubmissionImage, editSubmissionText, getSubmissions, sendSubmission } from "../controllers/submissionController.js";
+import { deleteSubmission, editSubmissionImage, editSubmissionText, getSubmissions, sendSubmission, vote } from "../controllers/submissionController.js";
 import { uploadImage } from "../middleware/uploadImage.js";
 
 export const submissionRoute = Router();
@@ -10,3 +10,5 @@ submissionRoute.get('/submissions/:competitionId', checkAuth, getSubmissions);
 submissionRoute.put('/submissions/:competitionId/:userId/explanation',checkAuth,editSubmissionText);
 submissionRoute.put('/submissions/:competitionId/:userId/image', checkAuth, uploadImage.single('image'), editSubmissionImage);
 submissionRoute.delete('/submissions/:competitionId/:userId', checkAuth, deleteSubmission);
+
+submissionRoute.post('/submissions/vote', checkAuth, vote);
