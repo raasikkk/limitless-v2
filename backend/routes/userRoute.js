9aa87816695ab783183c490b0df6b796c001e3b5
Amankos Ansar <zugzwanggg0@gmail.com>
2025-04-29 18:14:07 +0500

    temporary commenting getUser by username

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -6,6 +6,6 @@ import { uploadImage } from "../middleware/uploadImage.js";
 export const userRoute = Router();
 
 userRoute.patch('/users/:id/avatar', uploadImage.single('avatar'), editUserAvatar);
-userRoute.get('/users/:username', checkAuth, getUserByUsername);
+// userRoute.get('/users/:username', checkAuth, getUserByUsername);
 userRoute.get('/users/:id', checkAuth, getUserByUserId);
 userRoute.put('/users/:id', checkAuth, editUserById)
