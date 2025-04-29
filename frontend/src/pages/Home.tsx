e2bd1d7dcc4c97a397f42624c642808e53344e2a
Amankos Ansar <zugzwanggg0@gmail.com>
2025-04-29 17:25:28 +0500

    welcome real username

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -27,14 +27,14 @@ const visiters = [
 
 const Home = () => {
   const { t } = useTranslation()
-  const {isLogged} = useAppSelector((state)=>state.user)
+  const {isLogged, user} = useAppSelector((state)=>state.user)
   
   return (
     <div className="text-black dark:text-white ">
       {isLogged ? (
         <div className="pt-10 flex flex-wrap lg:flex-nowrap justify-between gap-3">
           <div className="pt-10 lg:pt-20 w-full lg:w-1/2 flex flex-col gap-3">
-            <h1 className="font-bold text-4xl md:text-5xl">{t('welcome')}, raasikkk!</h1>
+            <h1 className="font-bold text-4xl md:text-5xl">{t('welcome')}, {user?.username}!</h1>
             {/* <LanguageSwitcher /> */}
             <p className="lg:w-2/3 text-lg">{t("motivational_phrase")}</p>
 
