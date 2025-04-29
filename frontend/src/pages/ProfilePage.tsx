2e877d2a64149220fb93c11297155d213ab81a3f
Amankos Ansar <zugzwanggg0@gmail.com>
2025-04-29 18:08:44 +0500

    fixing no overload matched call in profilePage

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -49,7 +49,9 @@ const ProfilePage = () => {
                     <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold text-black dark:text-white">{userData?.username}</h2>
                     <p className="text-xs sm:text-sm lg:text-base text-gray-600 dark:text-white flex flex-col sm:flex-row items-center gap-2">
                         <CalendarDays className="hidden sm:inline-block" />
-                        Joined {formatDistanceToNow(new Date(userData?.created_at), { addSuffix: true })}
+                        {userData?.created_at && (
+                          <>Joined {formatDistanceToNow(new Date(userData.created_at), { addSuffix: true })}</>
+                        )}
                     </p>
                 </div>
             </div>
