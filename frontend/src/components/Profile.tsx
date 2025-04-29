47f9eda8c57f8ff444bd6b63e185e381a3c517e1
Amankos Ansar <zugzwanggg0@gmail.com>
2025-04-29 17:23:20 +0500

    adding real data to user profile component

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -2,24 +2,26 @@ import { useNavigate } from "react-router-dom"
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuTrigger } from "./ui/dropdown-menu"
 import { LogOut, User } from "lucide-react"
+import { useAppSelector } from "@/hooks/hooks"
 
 
 const Profile = () => {
     const navigate = useNavigate()
+    const {user} = useAppSelector((state)=>state.user);
     return (
     <>
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Avatar>
-                    <AvatarImage src="/ava.jpg"/>
+                    <AvatarImage src={user?.avatar}/>
                     <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-64 bg-[#1E1E1E] text-[#FAFAFA] border-[#2A252C]">
-                <DropdownMenuLabel><p className="p-1">My Account</p></DropdownMenuLabel>
+                <DropdownMenuLabel><p className="p-1">{user?.username}</p></DropdownMenuLabel>
                 <DropdownMenuSeparator className=""  />
                 {/* <DropdownMenuGroup> */}
-                    <DropdownMenuItem onClick={() => navigate("/profile/1")} className="hover:bg-[#2A252C] flex items-center gap-2">
+                    <DropdownMenuItem onClick={() => navigate(`/profile/${user?.id}`)} className="hover:bg-[#2A252C] flex items-center gap-2">
                         <User />
                         <p className="text-sm truncate">Profile</p>
                         <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
