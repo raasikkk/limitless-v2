5cbb44e1be5f95a3e8151d52070577b22e41fc68
Amankos Ansar <zugzwanggg0@gmail.com>
2025-05-04 12:50:39 +0500

    suggestion handler

--- frontend/src/components/SuggestionPopup.tsx
+++ frontend/src/components/SuggestionPopup.tsx
@@ -1,7 +1,10 @@
 import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { useAppSelector } from "@/hooks/hooks"
+import axios from "axios"
 import { Bot } from "lucide-react"
+import { useState } from "react"
 // import { useState } from "react"
-import { useTranslation } from "react-i18next"
+import { Loader2 } from "lucide-react"
 
 // const suggestions = [
 
@@ -9,17 +12,48 @@ import { useTranslation } from "react-i18next"
 
 
 const SuggestionPopup = () => {
-    const { t } = useTranslation()
+    const {user} = useAppSelector((state) => state?.user);
+    const [suggestion, setSuggestion] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleGetSuggestions = async () => {
+      setIsLoading(true);
+      try {
+
+        const suggestion = (await axios.get(`${import.meta.env.VITE_BACKEND_BASE_URL}/llm/advice/${user?.id}`)).data;
+        setSuggestion(suggestion.response)
+        
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false)
+      }
+    }
 
   return (
     <Dialog>
-          <DialogTrigger className="flex text-white items-center gap-2 fixed bottom-5 right-5 p-2 px-4 font-semibold bg-primaryColor rounded-md animate-bounce duration-1000 opacity-90">
+          <DialogTrigger onClick={handleGetSuggestions} className="flex text-white items-center gap-2 fixed bottom-5 right-5 p-2 px-4 font-semibold bg-primaryColor rounded-md animate-bounce duration-1000 opacity-90">
             <Bot />
-            Suggestions
+            {
+              isLoading
+              ?
+              <Loader2 className="animate-spin"/>
+              :
+              'Suggestions'
+            }
           </DialogTrigger>
           <DialogContent className="sm:max-w-[500px] ">
             <DialogHeader>
               <DialogTitle>Suggestion Based on Bio</DialogTitle>
+              <div className="py-4">
+              {
+                isLoading
+                ?
+                <Loader2 className="animate-spin"/>
+                :
+                suggestion
+              }
+              </div>
             </DialogHeader>
 
             <div className="flex h-40 items-center justify-center">
@@ -30,7 +64,7 @@ const SuggestionPopup = () => {
                 
               <div className="flex items-center gap-2">
                 <DialogClose asChild>
-                    <button className="border p-2 px-3 rounded-md">{t("cancel")}</button>
+                    <button className="border p-2 px-3 rounded-md">Okay</button>
                 </DialogClose>
               </div>
             </div>
