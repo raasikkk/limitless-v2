00e73c36eafa2673ef855add289d6ac38c31a2e0
Amankos Ansar <zugzwanggg0@gmail.com>
2025-05-08 10:18:29 +0500

    feedback table

--- backend/models.sql
+++ backend/models.sql
@@ -85,3 +85,12 @@ CREATE TABLE votes (
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (submission_id) REFERENCES submissions(id)
 )
+
+CREATE TABLE feedback (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  subject VARCHAR(500),
+  message TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)
